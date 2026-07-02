8a38ff214a4c3385c725a43af9b3218c53b4ff49
habakuki-official <habacucog77@gmail.com>
2026-07-02 19:57:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ function goToRegister(){
 function goToLogin(){
     window.location.href = "login.html";
 }
+function adminLogin(){
+
+let username=document.getElementById("username").value;
+let password=document.getElementById("password").value;
+
+if(username==="admin" && password==="123456"){
+
+window.location.href="admin-dashboard.html";
+
+}else{
+
+alert("Username cyangwa Password siyo.");
+
+}
+
+}
